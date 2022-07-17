042a6de3b42144b0bf788fe4d7f3bf5e0f0dbb43
TheSerch07 <jejog50@gmail.com>
2022-07-16 19:10:41 -0500

    backend

--- api/src/handles/funcions.js
+++ api/src/handles/funcions.js
@@ -1,1 +1,7 @@
-const { Videogame, Genre } = require("../db.js")
+const { Videogame, Genre } = require("../db.js");
+const axios = require("axios");
+
+async function getVideogames() {
+    let arrayGames = []
+    const info = await axios("")
+}
