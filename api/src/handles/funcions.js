fc3ca72196bd3ef8d7d57463521692801ba06920
TheSerch07 <jejog50@gmail.com>
2022-07-18 18:51:00 -0500

    backend

--- api/src/handles/funcions.js
+++
@@ -1,7 +0,0 @@
-const { Videogame, Genre } = require("../db.js");
-const axios = require("axios");
-
-async function getVideogames() {
-    let arrayGames = []
-    const info = await axios("")
-}
---
+++ api/src/handles/functions.js
@@ -0,0 +1,69 @@
+const { Videogame, Genre } = require("../db.js");
+const axios = require("axios");
+const { Op } = require("sequelize");
+
+function videogameInfo(data) {
+    return {
+        id: data.id,
+        name: data.name,
+        image: data.background_image,
+        genre: data.genres.map((genre) => genre.name),
+        releaseDate: data.released,
+        rating: data.rating,
+        platforms: data.platforms.map((platform) => platform.platform.name)
+    }
+};
+
+async function getVideogamesApi() {
+    let arrayGames = []
+    const info = await axios("https://api.rawg.io/api/games?key=22b7d9c1190846e38e66003610885078")
+    // console.log(info.data.results)
+    info.data.results.forEach((game) => {
+        arrayGames.push(videogameInfo(game))
+    })
+    console.log(arrayGames)
+    return arrayGames
+};
+
+async function getVideogamesApiName(name) {
+    let arrayGames = []
+    const infoForName = await axios("https://api.rawg.io/api/games?search=" + name + "&key=22b7d9c1190846e38e66003610885078")
+    infoForName.data.results.forEach((game) => {
+        arrayGames.push(videogameInfo(game))
+    })
+    console.log(arrayGames, "jeje")
+    return arrayGames
+};
+
+async function getVideogamesApiId(id) {
+    const infoForId = await axios("https://api.rawg.io/api/games/"+ id + "?key=22b7d9c1190846e38e66003610885078")
+    console.log(videogameInfo(infoForId.data), "añañay")
+    return videogameInfo(infoForId.data)
+};
+
+async function getVideogamesDb() {
+    const videogameFromDb = await Videogame.findAll({
+        include: {
+            model: Genre,
+            attribures: ["name"]
+        }
+    })
+    return videogameFromDb
+};
+
+async function getVideogamesDbName(name) {
+    const videogameDbName = await Videogame.findAll({
+        include: Genre,
+        where: {
+            name: {
+                [Op.iLike]: `%${name}`
+            }
+        }
+    })
+    return videogameDbName
+};
+
+
+console.log(getVideogamesApi())
+// console.log(getVideogamesApiName("halo"))
+// console.log(getVideogamesApiId(339268))
