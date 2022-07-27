0cf1b51b89b90837b64f25649779f562f9bcde05
TheSerch07 <jejog50@gmail.com>
2022-07-26 20:22:45 -0500

    frontend

--- client/src/components/CreateVideogame.jsx
+++ client/src/components/CreateVideogame.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { postVideogame, fetchGenres } from "../store/actions";
+import { useDispatch, useSelector } from "react-redux"
+
 export default function CreateVideogame() {
     return (
         <h1>I'm route create</h1>
